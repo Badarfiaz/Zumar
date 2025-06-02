ded9871a5a1e1e76ce9e7d14d8b816b34ab6ff2d
Abdulazizchughtai <abdulazizchughtai793@gmail.com>
2025-06-02 16:54:56 +0500

    dummy

--- e-com/src/pages/Landing.jsx
+++ e-com/src/pages/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
   return (
     <>
          <Banner
-        title="Welcome to Shirtopia"
+        title="Welcome to Shirtopiaaaa"
         subtitle="Discover premium t-shirts that match your vibe."
         imageUrl="https://images.unsplash.com/photo-1541099649105-f69ad21f3246"
         buttonText="Shop Now"
